859b57581c44058273035d2eb1421355b95b57e8
_c0da_ <duvictor514@gmail.com>
2018-07-15 20:52:43 -0700

    Removed unused import.

--- app/src/main/java/com/team3543/awooclient/MainActivity.java
+++ app/src/main/java/com/team3543/awooclient/MainActivity.java
@@ -2,7 +2,6 @@ package com.team3543.awooclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
